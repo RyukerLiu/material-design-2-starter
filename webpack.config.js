9fc925f45c9794688492c596adac4e3e55b317f6
Allen Liu <alex03108861@gmail.com>
2023-05-15 15:24:30 +0800

    Step3

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,17 @@
 const autoprefixer = require('autoprefixer');
 
-module.exports = [{
-    entry: './app.scss',
+module.exports = {
+    mode: 'development',
+    entry: ['./app.scss', './app.js'],
     output: {
-        // This is necessary for webpack to compile
-        // But we never use style-bundle.js
-        filename: 'style-bundle.js',
+        filename: 'bundle.js',
     },
     module: {
         rules: [
             {
                 test: /\.scss$/,
                 use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: 'bundle.css',
-                        },
-                    },
-                    { loader: 'extract-loader' },
+                    { loader: 'style-loader' },
                     { loader: 'css-loader' },
                     {
                         loader: 'postcss-loader',
@@ -26,35 +19,32 @@ module.exports = [{
                             postcssOptions: {
                                 plugins: [
                                     autoprefixer()
-                                ]
-                            }
-                        }
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            // Prefer Dart Sass
-                            implementation: require('sass'),
-
-                            // See https://github.com/webpack-contrib/sass-loader/issues/804
-                            webpackImporter: false,
+                                ],
+                            },
                         },
                     },
                     {
                         loader: 'sass-loader',
                         options: {
-                            // Prefer Dart Sass
                             implementation: require('sass'),
-
-                            // See https://github.com/webpack-contrib/sass-loader/issues/804
                             webpackImporter: false,
                             sassOptions: {
-                                includePaths: ['./node_modules']
+                                includePaths: ['./node_modules'],
                             },
-                        }
+                        },
                     },
-                ]
-            }
-        ]
+                ],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
     },
-}];
+};
