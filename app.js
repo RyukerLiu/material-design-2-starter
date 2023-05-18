897ddba53f4b8a8f6a7a4e2eb186f045fde01a55
Allen Liu <alex03108861@gmail.com>
2023-05-18 17:06:33 +0800

    All Buttons

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 import { MDCRipple } from '@material/ripple';
+import { MDCIconButtonToggle } from '@material/icon-button';
 
-const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
+const buttons = document.querySelectorAll('.mdc-button');
+buttons.forEach((button) => {
+    new MDCRipple(button);
+});
+
+const icon_buttons = document.querySelectorAll('.mdc-icon-button');
+icon_buttons.forEach((button) => {
+    new MDCIconButtonToggle(button);
+});
+
+icon_buttons.forEach((button) => {
+    new MDCRipple(button);
+});
+
