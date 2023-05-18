2e537727e05ba8620b4318140238d14fae4d3619
Allen Liu <alex03108861@gmail.com>
2023-05-18 15:48:00 +0800

    Button

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
-import { MDCRipple } from '@material/ripple/index';
-const ripple = new MDCRipple(document.querySelector('.foo-button'));
+import { MDCRipple } from '@material/ripple';
+
+const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
